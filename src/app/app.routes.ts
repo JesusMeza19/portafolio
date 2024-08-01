3468887a6c0f2a5a18dc7a8db6dd5df682c6ae49
Jesus Emmanuel Meza Yarto <jesus.meza@talisis.com>
2024-08-01 17:14:34 -0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//componentes
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+
+
+export const routes: Routes = [
+    {path: '', component: HabilidadesComponent, pathMatch: 'full'}
+];
